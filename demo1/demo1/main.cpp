0e9f7b65157fb2e48a9c05bc09b9d2e840f20e8b
Nathan Tadych <nathan87007@gmail.com>
2012-09-05 14:00:38 -0500

    assignment2TextFormatting

---
+++ demo1/demo1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	cout << "\ayour string\n";
+	cout <<	"\tthis is a new line!";
+	cout << "\x0003";
+	
+	getch();
+	return 0;
+}
