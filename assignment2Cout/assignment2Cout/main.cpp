ffbcf186f6178790023814532e9ccf66fd6a5917
Nathan Tadych <nathan87007@gmail.com>
2012-09-10 03:57:15 -0500

    assignment2Cout

---
+++ assignment2Cout/assignment2Cout/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	//Display the verse in proper format 
+	cout << "Computers, computers everywhere\n";
+	cout << "\tas far as I can C.\n";
+	cout << "I really, really like these things\n";
+	cout << "\tOh joy. Oh joy for me!";
+	//Allow the program to run until a key is pressed
+	getch();
+}
