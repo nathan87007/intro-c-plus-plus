06ef9612357a97d5bf865f184b5e2c5b13a8ce41
Nathan Tadych <nathan87007@gmail.com>
2012-10-03 13:48:35 -0500

    update

---
+++ assignment7Salary/assignment7Salary/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int hours = 0;
+	
+	
+	cout << "Please enter the amount of hours worked: ";
+	cin >> hours;
+
+
+
+	_getch();
+	return 0;
+}
