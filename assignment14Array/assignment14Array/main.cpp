4b57fbfdef1218506c9b5f950ce08712d9a9ef93
Nathan Tadych <nathan87007@gmail.com>
2012-10-24 14:05:25 -0500

    arrays

---
+++ assignment14Array/assignment14Array/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int first[2][3] = {{16,18,23},{54,91,11}};
+	int second[2][3] = {{14,52,77},{16,19,59}};
+
+	cout << first << endl;
+	cout << second << endl;
+
+
+	_getch();
+	return 0;
+}
