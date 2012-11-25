42426008b767fb42d5ba26f500c018c0d28ff45c
Nathan Tadych <nathan87007@gmail.com>
2012-11-25 00:18:26 -0600

    update assignment 14

--- assignment14Array/assignment14Array/main.cpp
+++ assignment14Array/assignment14Array/main.cpp
@@ -7,13 +7,38 @@ using namespace std;
 
 int main()
 {
-	int first[2][3] = {{16,18,23},{54,91,11}};
-	int second[2][3] = {{14,52,77},{16,19,59}};
+	int first [2][3] = {{16,18,23},{54,91,11}};
+	int second [2][3] = {{14,52,77},{16,19,59}};
+	
+	//display the first array
+	cout << "The first array:";
+	for(int x=0; x<2; x++){
+		cout << endl;
+		for(int y=0; y<3; y++){
+			cout << first [x][y] << " ";
+		}
+	}
+	cout << endl;
 
-	cout << first << endl;
-	cout << second << endl;
+	//display the second array
+	cout << "The second array:";
+	for(int x=0; x<2; x++){
+		cout << endl;
+		for(int y=0; y<3; y++){
+			cout << second [x][y] << " ";
+		}
+	}
+	cout << endl;
 
-
+	//display sum of two arrays
+	cout << "The two arrays added together:";
+	for(int x=0; x<2; x++){
+		cout << endl;
+		for(int y=0; y<3; y++){
+			cout << second [x][y]+first [x][y] << " ";
+		}
+	}
+	cout << endl;
 	_getch();
 	return 0;
 }
